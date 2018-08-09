c66610a140907cc96b6806ff870154c386e77264
Atom94 <elvismurte@gmail.com>
2018-08-09 15:05:50 +0200

    09/08

--- PROJECTELVIS/php/validatelogin.php
+++ PROJECTELVIS/php/validatelogin.php
@@ -1,4 +1,8 @@
 <?php
+// Start the session
+session_start();
+?>
+<?php
 include '../php/connessionedb.php';
 // Grab User submitted information
 $user = $_POST["username"];
@@ -8,9 +12,17 @@ $result=$DB->query($sqll);
 $row=$result->fetch_assoc();
 $user = mysqli_real_escape_string($DB,$user);
 $pass= mysqli_real_escape_string($DB,$pass);
+$cam="SELECT campionato from squadra where nome COLLATE UTF8_GENERAL_CI LIKE '$squadra'";
+	$camp=$DB->query($cam);
+$row1=$camp->fetch_assoc();
+		$idc=$row1["campionato"];
 		if(	$row["username"]==$user && $row["password"]==$pass	)
 		{
-			header('Location: ../php/squadra.php?squadra=".$row["squadrapref"]."');
+// Set session variables
+$_SESSION["$user"] = "green";
+$_SESSION["$pass"] = "cat";
+echo "Session variables are set.";
+			header('Location: ../php/squadra.php?squadra=$row["squadrapref"]&idc=$row1["campionato"]');
 		}
 	else echo"errore,credenziali errate";
 	?>
